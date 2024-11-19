cd14d1c2a1ecdfa3455866d569f2b62faff47c2e
ShrutiKathiriya-coder <kathiriyashruti3@gmail.com>
2024-11-19 19:07:33 +0530

    Add files via upload

---
+++ output.js
@@ -0,0 +1,25 @@
+//output//
+
+// 4 type of output
+
+    // 1.console.log
+    // 2.alert
+    // 3.document.write
+    // 4.dom
+
+// 1.console.log
+
+console.log("shruti");
+console.warn("you are disturb me")
+
+//2. alert
+
+alert("shruti kathiriya")
+
+//3.document.write
+
+document.write("hello world")
+
+//4.dom
+
+document.getElementById("out").innerHTML;
